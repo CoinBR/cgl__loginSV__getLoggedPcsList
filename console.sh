4f1eb814d3494b185f70408bf48ebdafdc6a8017
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-27 13:26:17 -0300

    Fix submodules structure

--- console.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-if (( $EUID != 0 )); then
-  docker-compose run sgl-login-sv-get-pcs-list bash 
-  exit
-fi
-docker-compose run -u 0 sgl-login-sv-get-pcs-list bash 
-
